c4950b784645ea415791ecd13d75e64c58e5f78c
Shivakumar9849 <skumar45072@gmail.com>
2024-02-29 21:26:54 +0530

    python

--- input.py
+++ input.py
@@ -1,2 +1,11 @@
-shiva=bool(-5)
-print(shiva)
+n=int(input())
+c=0
+print(n)
+while n!=0:
+    d=n%10
+    c+=1
+    if d==7 or n%7==0:
+        print('Buzz number')
+    else:
+        print('not a Buzz number')
+    n=n//10
